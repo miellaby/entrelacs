893ea748fd315f27411e1ddcbf191538da7e9806
sylvain.garden <sylvain.garden@479a42bd-9052-0410-8fdd-918bd02b771f>
2011-06-22 14:38:16 +0000

    testu

--- testXYZ.c
+++ testXYZ.c
@@ -1,12 +1,15 @@
 #include "entrelacs/entrelacs.h"
 #include <stdlib.h> // free & co
 #include <stdio.h>  // sprintf & co
-
+#include <assert.h>
 int main(int argc, char* argv[]) {
 	xl_init();
+       char output[255];
 	char *s1, *s2, *s3;
 	Arrow helloWorld = xl_arrow(xl_tag("hello"), xl_tag("world"));
-	sprintf("%s %s", s1 = xl_tagOf(xl_tailOf(helloWorld)), s2 = xl_tagOf(xl_headOf(helloWorld)));
+	snprintf(output, 255, "%s %s", s1 = xl_tagOf(xl_tailOf(helloWorld)), s2 = xl_tagOf(xl_headOf(helloWorld)));
+       puts(output);
+	assert(0 == strcmp(output, "hello world"));
 	free(s1);
 	free(s2);
 	
