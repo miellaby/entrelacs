40424feac92d5845811da85fa2b9468cbe9cc396
sylvain.garden <sylvain.garden@479a42bd-9052-0410-8fdd-918bd02b771f>
2011-06-23 14:52:09 +0000

    first test passed. Big step.

--- trunk/mem0.h
+++ trunk/mem0.h
@@ -6,15 +6,17 @@
 #include <sys/types.h>
 
 typedef uint64_t Cell;
-typedef uint64_t CellData;
-typedef uint32_t Address;
+typedef uint64_t CellData; // Actually up to 7 bytes max
+typedef uint32_t Address; // Actually 3 bytes (24 bits) max
 
 // initialization
 int mem0_init(); // return !0 if very first init
 
-// Twin Prims prevent clustering when open-addressing
+// Twin Prims are said to prevent data clustering when open-addressing
 // The nearest twin prims lower than 8388608 (2^23) is 2^23-157 and 2^23-159
-// Thanks to http://primes.utm.edu/lists/2small/0bit.html
+//     thanks to http://primes.utm.edu/lists/2small/0bit.html
+// Note: one obtains a 8MegaCell level-0 memory bank.
+//       it should allow to store few billions of arrows
 
 #define _PRIM0 ((1 << 23) - 157)
 
