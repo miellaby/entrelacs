66deca00473b655496d41ed6872eaa7d35804ea0
sylvain.garden <sylvain.garden@479a42bd-9052-0410-8fdd-918bd02b771f>
2011-06-23 16:05:36 +0000

    connection fixing

--- trunk/testXYZ.c
+++ trunk/testXYZ.c
@@ -5,14 +5,19 @@
 int main(int argc, char* argv[]) {
 	xl_init();
     char output[255];
-	char *s1, *s2, *s3;
 	Arrow helloWorld = xl_arrow(xl_tag("hello"), xl_tag("world"));
-	snprintf(output, 255, "%s %s", s1 = xl_tagOf(xl_tailOf(helloWorld)), s2 = xl_tagOf(xl_headOf(helloWorld)));
+	char *s1 = xl_tagOf(xl_tailOf(helloWorld));
+	char *s2 = xl_tagOf(xl_headOf(helloWorld));
+	snprintf(output, 255, "%s %s", s1, s2);
     puts(output);
 	assert(0 == strcmp(output, "hello world"));
 	free(s1);
 	free(s2);
     xl_root(helloWorld);
 	xl_commit();
+	assert(xl_isRooted(helloWorld));
+	xl_unroot(helloWorld);
+	assert(!xl_isRooted(helloWorld));
+	
 	return 0;
 }
