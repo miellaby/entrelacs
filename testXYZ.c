eaf1562df8912f59252aa0dc15ac0eb815e5d1d3
sylvain.garden <sylvain.garden@479a42bd-9052-0410-8fdd-918bd02b771f>
2011-06-26 21:30:31 +0000

    fixing

--- testXYZ.c
+++ testXYZ.c
@@ -3,6 +3,47 @@
 #include <stdio.h>  // sprintf & co
 #include <assert.h>
 #include <string.h>
+#include "space.h"
+struct s_printArrowC { int size; int max; char* buffer; };
+
+int printArrow(Arrow a, void *ctx) {
+    struct s_printArrowC *context = (struct s_printArrowC *)ctx;
+    fprintf(stderr, "Arrow %06x ", a);
+    if (!context) { 
+      struct s_printArrowC context0 = { 0, 0, NULL };
+      geoalloc(&context0.buffer, &context0.max, &context0.size, sizeof(char), 1);
+      context0.buffer[0] = '\0';
+      printArrow(a, (void *)&context0);
+      fprintf(stderr, "%s\n", context0.buffer);
+      return 0;
+    }
+
+    char *s;
+    enum e_xlType t = xl_typeOf(a);
+    if (t == XL_TAG) {
+        int l;
+        char* s = xl_btagOf(a, &l);
+        int size = context->size;
+        geoalloc(&context->buffer, &context->max, &context->size, sizeof(char), size + l - 1 + 2);
+        sprintf(context->buffer + size - 1, "\"%s\"", s);
+        free(s);
+    } else if (t == XL_ARROW) {
+        int size;
+        size = context->size;
+        geoalloc(&context->buffer, &context->max, &context->size, sizeof(char), size + 1);
+        sprintf(context->buffer + size - 1, "(");
+        printArrow(xl_tailOf(a), (void *)context);
+        size = context->size;
+        geoalloc(&context->buffer, &context->max, &context->size, sizeof(char), size + 2);
+        sprintf(context->buffer + size - 1, ", ");
+        printArrow(xl_headOf(a), (void *)context);
+        size = context->size;
+        geoalloc(&context->buffer, &context->max, &context->size, sizeof(char), size + 1);
+        sprintf(context->buffer + size - 1, ")");
+    }
+    return 0;
+}
+
 int main(int argc, char* argv[]) {
 	xl_init();
     char output[255];
@@ -17,8 +58,13 @@ int main(int argc, char* argv[]) {
     xl_root(helloWorld);
 	xl_commit();
 	assert(xl_isRooted(helloWorld));
-	xl_unroot(helloWorld);
+    Arrow helloDude = xl_arrow(xl_tag("hello"), xl_tag("dude"));
+    xl_root(helloDude);
+    xl_childrenOf(xl_tag("hello"), printArrow, NULL);
+    xl_unroot(helloWorld);
 	assert(!xl_isRooted(helloWorld));
+    xl_childrenOf(xl_tag("hello"), printArrow, NULL);
+    xl_unroot(helloDude);
 	xl_commit();
 
 	return 0;
