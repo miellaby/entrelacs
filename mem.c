e4c2f3f42a7ad79f9a45d7ccec21f67c264babba
sylvain.garden <sylvain.garden@479a42bd-9052-0410-8fdd-918bd02b771f>
2012-01-16 08:24:11 +0000

    no more useless volatile flags in mem.h accessors

--- mem.c
+++ mem.c
@@ -1,4 +1,9 @@
-#define SPACE_C
+/** @file
+ Orthogonally persistent memory device.
+ It basically consists in a software implemented direct-mapped RAM cache with buffering capacity in front of mem0.
+ Changed cells in the current micro-transaction are buffered until a commit is performed (it's neither a write-trough nor a 'lazy write' cache).
+ */
+#define MEM_C
 #include <stdlib.h>
 #include <assert.h>
 #include "mem.h"
@@ -10,45 +15,49 @@
   #define ONDEBUG(w)
 #endif
 
-// The entrelacs space code.
-// It consists in a RAM cache in front of the memory 0 level storage.
-// Todo: Cuckoo hashing
 #define MEMSIZE 0x2000
-#define RESERVESIZE 1024
-static const Address memSize = MEMSIZE ; // cache size (8192)
-static const Address reserveSize = RESERVESIZE ; // cache reserve size
+static const Address memSize = MEMSIZE ; ///< mem size (8192 records)
 
-// The RAM cache : an n="size" array of record. Each record can carry one mem0 cell.
+/** The main RAM cache, aka "mem".
+ mem is an array of MEMSIZE records.
+ Each record can carry one mem0 cell.
+ */
 static struct s_mem {
-  unsigned short a; // <--page # (12 bits)--><--mem1admin (3 bits)--><--CHANGED (1 bit)-->
+  unsigned short a; ///< <--mem1 internal flags (4 bits)--><--page # (12 bits)-->
   Cell c;
 } m, mem[MEMSIZE];
 
+#define RESERVESIZE 1024
+static const Address reserveSize = RESERVESIZE ; ///< cache reserve size
 
-// The RAM cache reserve.
-// Modified cells are put into this reserve when their location are needed to load other cells
+/** The RAM cache reserve.
+ Modified cells are moved into this reserve when their location in mem[] are need to load other cells
+ TODO replace it by cuckoo hashing or smarter alternative
+ */
 static struct s_reserve {
-  Address a; // <--padding (4 bits)--><--cell address (24 bits)--><--mem1admin (3 bits)--><-- CHANGED (1 bit) -->
+  Address a;
   Cell    c;
-} reserve[RESERVESIZE]; // cache reserve stack
-static uint32_t reserveHead = 0; // cache reserve stack head
+} reserve[RESERVESIZE];
 
-// The change log.
-// This is a log of all changes made in cached memory.
-static Address* log = NULL;
-static size_t   logMax = 0;
-static size_t   logSize = 0;
+static uint32_t reserveHead = 0; ///< cache reserve stack head
+
+/** The change log.
+ This is a log of all changes made in cached memory. It grows geometrically (see geoalloc() function).
+ */
+static Address* log = NULL; ///< dynamically allocated
+static size_t   logMax = 0; ///< heap allocated log size
+static size_t   logSize = 0; ///< significative log size
 
 
 
-#define MEM1_CHANGED 0x1
-#define MEM1_EMPTY   0xE
-#define memPageOf(M) ((M).a >> 4)
+#define MEM1_CHANGED 0x1000
+#define MEM1_EMPTY   0xE000
+#define memPageOf(M) ((M).a & 0x0FFF)
 #define memIsEmpty(M) ((M).a == MEM1_EMPTY)
 #define memIsChanged(M) ((M).a & MEM1_CHANGED)
 
 
-// you don't want to know
+/** set-up or reset a tuple {allocated memory, allocated size, significative size} used by geoalloc() function */
 void zeroalloc(char** pp, size_t* maxp, size_t* sp) {
   if (*maxp) {
     free(*pp);
@@ -58,7 +67,7 @@ void zeroalloc(char** pp, size_t* maxp, size_t* sp) {
   *sp = 0;
 }
 
-// you don't want to know
+/** geometricaly grow a heap allocated memory area by providing a tuple {allocated memory, allocated size, current significative size} and a new significative size */
 void geoalloc(char** pp, size_t* maxp, size_t* sp, size_t us, size_t s) {
   if (!*maxp)	{
       *maxp = (s < 128 ? 256 : s + 128 );
@@ -75,38 +84,12 @@ void geoalloc(char** pp, size_t* maxp, size_t* sp, size_t us, size_t s) {
   return;
 }
 
-/* Get memory level 1 meta-data associated with a cell */
-uint32_t mem_getAdmin(Address a) {
-  ONDEBUG((fprintf(stderr, "mem_getAdmin@%06x = ", a)));
-  int i;
-  Address offset = a & 0xFFF;
-  uint32_t  page   = a >> 12;
-  m = mem[offset];
-  if (!memIsEmpty(m) && memPageOf(m) == page) {
-    uint32_t mem1admin =  (m.a & 0xF) >> 1;
-    ONDEBUG((fprintf(stderr, "%01x from cache\n", mem1admin)));
-    return mem1admin;
-  }
-  
-  for (i = 0; i < reserveHead ; i++) {
-    if ((reserve[i].a >> 4) == a) {
-      uint32_t mem1admin = (reserve[i].a & 0xF) >> 1;
-      ONDEBUG((fprintf(stderr, "%01x from reserve\n", mem1admin)));
-      return mem1admin;
-    }
-  }
-
-  // for a newly loaded cell: mem1admin = 0;
-  ONDEBUG((fprintf(stderr, "0 because not cached\n")));  
-  return 0;
-}
-
-/* Get a cell */
+/** Get a cell */
 Cell mem_get(Address a) {
   ONDEBUG((fprintf(stderr, "mem_get@%06x = ", a)));
   Address i;
   Address offset = a & 0xFFF;
-  uint32_t  page   = a >> 12;
+  uint32_t  page = a >> 12;
   m = mem[offset];
   if (!memIsEmpty(m) && memPageOf(m) == page) {
     ONDEBUG((fprintf(stderr, "%016llx from cache\n", m.c)));
@@ -114,7 +97,7 @@ Cell mem_get(Address a) {
   }
   
   for (i = 0; i < reserveHead ; i++) {
-    if ((reserve[i].a >> 4) == a) {
+    if (reserve[i].a == a) {
       ONDEBUG((fprintf(stderr, "%016llx from reserve\n", reserve[i].c)));
       return reserve[i].c;
     }
@@ -122,101 +105,73 @@ Cell mem_get(Address a) {
 
   if (memIsChanged(m)) {
     // When replacing a modified cell, move it to reserve
-    Address moved = ((m.a & 0xFFF0) << 8) | offset;
+    Address moved = (memPageOf(m) << 12) | offset;
     ONDEBUG((fprintf(stderr, "(%06x moved in reserve) ", moved)));
     assert(reserveHead < reserveSize);
-    reserve[reserveHead].a = (moved << 4) | (m.a & 0xF) ;
+    reserve[reserveHead].a = moved;
     reserve[reserveHead++].c = m.c;
   }
 
-  mem[offset].a = page << 4; // for a newly loaded cell: mem1admin = 0;
+  mem[offset].a = page;
   return (mem[offset].c = mem0_get(a));
   // rem: debug trace end put by mem0_get
 }
 
-/* Set a cell */
-void mem_set(Address a, Cell c, uint32_t mem1admin) { // FIXME What if cell is in reserve?
-  ONDEBUG((fprintf(stderr, "mem_set@%06x : %016llx A1=%01x ", a, c, mem1admin)));
-  Address offset = a & 0xFFF;
-  uint32_t page    = a >> 12;
-  mem1admin = (mem1admin & 7) << 1; // Note one shift admin bits once left here
-  m = mem[offset];
-  if (memIsEmpty(m) || memPageOf(m) != page)  { // Uhh that's not fun
-    for (int i = reserveHead - 1; i >=0 ; i--) { // Look at the reserve
-      if ((reserve[i].a >> 4) == a) {
-         ONDEBUG((fprintf(stderr, "(found in reserve)\n")));
-		 // one changes data in the reserve cell
-		 if (mem1admin != DONTTOUCH)
-  		     reserve[i].a = a << 4 | mem1admin | MEM1_CHANGED;
-		 reserve[i].c = c;
-		 return; // no need to log it (it's already done)
-      }
+/** Set a cell */
+void mem_set(Address a, Cell c) {
+    ONDEBUG((fprintf(stderr, "mem_set@%06x : %016llx", a, c)));
+    Address offset = a & 0xFFF;
+    uint32_t  page = a >> 12;
+    m = mem[offset];
+    if (memIsEmpty(m) || memPageOf(m) != page)  { // Uhh that's not fun
+        for (int i = reserveHead - 1; i >=0 ; i--) { // Look at the reserve
+            if (reserve[i].a == a) {
+                ONDEBUG((fprintf(stderr, "(found in reserve)\n")));
+                // one changes data in the reserve cell
+                reserve[i].c = c;
+                return; // no need to log it (it's already done)
+            }
+        }
+        // no copy in the reserve, one puts the modified cell in mem0
+        if (memIsChanged(m)) { // one replaces a modificied cell that one moves to reserve
+            Address moved = (memPageOf(m) << 12) | offset;
+            ONDEBUG((fprintf(stderr, "(%06x moved in reserve) ", moved)));
+            assert(reserveHead < reserveSize);
+            reserve[reserveHead].a = moved;
+            reserve[reserveHead].c = m.c;
+            reserveHead++;
+        }
+        // No need to load the cell from mem0 since one overwrites it
+        m.a = 0;
     }
-	// no copy in the reserve, one puts the modified cell in mem0
-    if (memIsChanged(m)) { // one replaces a modificied cell that one moves to reserve
-       Address moved = ((m.a & 0xFFF0) << 8) | offset;
-       ONDEBUG((fprintf(stderr, "(%06x moved in reserve) ", moved)));
-       assert(reserveHead < reserveSize);
-       reserve[reserveHead].a = (moved << 4) | (m.a & 0xF);
-       reserve[reserveHead].c = m.c;
-       reserveHead++;
-	}
-	// No need to load the cell from mem0 since one overwrites it
-    m.a = 0; // previous mem1admin ignored.
-  }
-
-  if (!memIsChanged(m)) { // Log it
-    geoalloc((char **)&log, &logMax, &logSize, sizeof(Address), logSize + 1);
-    log[logSize - 1] = a;
-  }
-  
-  mem[offset].a = page << 4 | (mem1admin == DONTTOUCH ? (m.a & 0xF) : mem1admin) | MEM1_CHANGED;
-  mem[offset].c = c;
-  ONDEBUG((fprintf(stderr, "logSize=%d\n", logSize)));
-}
-
-
-void mem_setAdmin(Address a, uint32_t mem1admin) {
-  ONDEBUG((fprintf(stderr, "mem_setAdmin@%06x : %01x ", a, mem1admin)));
-  Address offset = a & 0xFFF;
-  uint32_t page    = a >> 12;
-  m = mem[offset];
-  if (!memIsEmpty(m) && memPageOf(m) == page) { // ideal case
-      if ((m.a >> 1 & 7) == mem1admin) {
-         ONDEBUG((fprintf(stderr, " nothing to do.\n")));
-	     return;
-	  }
-  
-      if (!memIsChanged(m)) { // Log it
+    
+    if (!memIsChanged(m)) { // Log it
         geoalloc((char **)&log, &logMax, &logSize, sizeof(Address), logSize + 1);
         log[logSize - 1] = a;
-      }
-      mem1admin = (mem1admin & 7) << 1;
-      mem[offset].a = page << 4 | mem1admin | MEM1_CHANGED;
-	  // FIXME actually, one should distinguish between admin-changed cell and really modified cell
-      ONDEBUG((fprintf(stderr, " in cache.\n")));
-  } else { // oww the pain
-      ONDEBUG((fprintf(stderr, " ==> mem_set(mem_get(...))\n", a, mem1admin)));
-	  if (mem_getAdmin(a) != mem1admin)
-         return mem_set(a, mem_get(a), mem1admin); // TODO Might be optimized
-  }
+    }
+
+    mem[offset].a = MEM1_CHANGED | page;
+    mem[offset].c = c;
+    ONDEBUG((fprintf(stderr, "logSize=%d\n", logSize)));
 }
 
 static int _addressCmp(const void *a, const void *b) {
   return (a == b ? 0 : (a > b ? 1 : -1));
 }
 
+/** commit */
 void mem_commit() {
   ONDEBUG((fprintf(stderr, "mem_commit (logSize = %d)\n", logSize)));
   Address i;
   if (!logSize) return;
 
   qsort(log, logSize, sizeof(Address), _addressCmp);
-
+  // TODO: optimize by commiting all reserved cells first then all logged cells found in mem
   for (i = 0; i < logSize; i++) {
     Address a = log[i];
+    Address offset = a & 0xFFF;
     mem0_set(a, mem_get(a));
-    mem[a & 0xFFF].a &= 0xFFFE;
+    mem[offset].a &= 0xEFFF; // reset changed flag for this offset (even if the changed cell is actually in reserve)
   }
   mem0_commit();
 
@@ -225,6 +180,9 @@ void mem_commit() {
   ONDEBUG((fprintf(stderr, "mem_commit done\n")));
 }
 
+/** init
+  @return 1 if very first start, <0 if error, 0 otherwise
+*/
 int mem_init() {
   ONDEBUG((fprintf(stderr, "mem_init (memSize = %d)\n", memSize)));
   int rc = mem0_init();
@@ -236,5 +194,5 @@ int mem_init() {
 
   geoalloc((char **)&log, &logMax, &logSize, sizeof(Address), 0);
 
-  return rc; // return >0 if very first start, <0 if error otherwise 0
+  return rc;
 }
--- mem.h
+++ mem.h
@@ -1,21 +1,38 @@
 #ifndef MEM_H
 #define MEM_H
-/*
- * Persistent memory device with RAM cache
+/** @file
+ Orthogonally persistent memory device.
  */
 #include "mem0.h"
 
-#define DONTTOUCH 0xFFFFFFFF
+/** "mem" initialization
+ @return 1 if very first start, <0 if error, 0 otherwise
+ */
+int  mem_init();
 
-int  mem_init(); // return !0 if very first start
-void mem_set(Address, Cell, uint32_t); // set
-Cell mem_get(Address); // get address
-void mem_commit(); // commit changes
+/** set binary content into memory cell
+ @param cell Address
+ @param Cell content
+ */
+void mem_set(Address, Cell);
 
-void mem_setAdmin(Address, uint32_t); // attach 3 admin bits to Address
-uint32_t mem_getAdmin(Address); // read 3 attached admin bits (0 if unmodified)
+/** get binary content from memory cell
+ @param cell Address
+ @return Cell content
+ */
+Cell mem_get(Address);
 
+/** close current micro-transaction,
+ by making all memory changes from last commit persistent
+ */
+void mem_commit();
+
+/** setup or reinitialize a geometrically growing RAM area
+ */
 void zeroalloc(char** pp, size_t* maxp, size_t* sp);
+
+/** change content size inside a geometrically growing RAM area
+ */
 void geoalloc(char** pp, size_t* maxp, size_t* sp, size_t us, size_t s);
 #endif /* MEM_H */
 
