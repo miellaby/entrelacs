1487411853c93671ba98a45044ac93bf15e9ca31
sylvain.garden <sylvain.garden@479a42bd-9052-0410-8fdd-918bd02b771f>
2012-07-10 22:56:27 +0000

    url

--- trunk/cli.sh
+++ trunk/cli.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 echo hit ^D to exit. >&2
-if [ -x /user/local/bin/wget ] ; then
-  alias wget=/usr/local/bin/wget
-fi
+
 COOKIES=cookies.tmp
+HEADERS=headers.tmp
 
     while read line
 do
    echo -n '>> '
-   wget --load-cookies=$COOKIES --save-cookies=$COOKIES --keep-session-cookies \
-     -q -O - "http://localhost:8008/$line"
+   curl -b $COOKIES -c $COOKIES -D $HEADERS \
+     -s "http://localhost:8008/$line"
    session=$(sed -ne '/session/ s/.*\t//p' "$COOKIES")
-   echo " << ["$session"]"
+   location=$(sed -ne '/Content-Location/ s/.* \([^\r]*\).*/\1/p' "$HEADERS")
+   echo " << s="\"$session\"" u="\"$location\"
+   #cat $HEADERS
 done
 
