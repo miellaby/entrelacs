380535c294e1fc776ba80681da7c1d2acb360516
sylvain.garden@gmail.com <sylvain.garden@gmail.com@479a42bd-9052-0410-8fdd-918bd02b771f>
2013-02-15 21:32:26 +0000

    Demo client

--- html-demo-client/js/plugins.js
+++ html-demo-client/js/plugins.js
@@ -22,3 +22,15 @@
 }());
 
 // Place any jQuery/helper plugins in here.
+(function($) {
+    var userAgent = navigator.userAgent.toLowerCase();
+
+    $.browser = {
+        version: (userAgent.match( /.+(?:rv|it|ra|ie)[\/: ]([\d.]+)/ ) || [0,'0'])[1],
+        safari: /webkit/.test( userAgent ),
+        opera: /opera/.test( userAgent ),
+        msie: /msie/.test( userAgent ) && !/opera/.test( userAgent ),
+        mozilla: /mozilla/.test( userAgent ) && !/(compatible|webkit)/.test( userAgent )
+    };
+
+})(jQuery);
