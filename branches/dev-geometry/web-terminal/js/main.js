8ef46d89f72bac615bc659d7f5370e0a85fdc1e9
sylvain.garden@gmail.com <sylvain.garden@gmail.com@479a42bd-9052-0410-8fdd-918bd02b771f>
2013-07-11 22:43:18 +0000

    wizard

--- branches/dev-geometry/web-terminal/js/main.js
+++ branches/dev-geometry/web-terminal/js/main.js
@@ -20,8 +20,8 @@ function init() {
     
     var center = function(elt) {
            var p = elt.position();
-           var x = p.left + elt.width() / 2 - ($(window).width() / 2);
-           var y = p.top + elt.height() / 2 - ($(window).height() / 2) - $('.wizard_box').height() * 0.9;
+           var x = p.left - ($(window).width() / 2);
+           var y = p.top - elt.height() * 1.5 - ($(window).height() / 2) - $('.wizard_box').height() * 0.2;
            $('html, body').animate({scrollLeft: x, scrollTop: y}, 1000);
  
     };
