192fa60c8c0b327dea8f36b50e908e96ae55f3c2
sylvain.garden@gmail.com <sylvain.garden@gmail.com@479a42bd-9052-0410-8fdd-918bd02b771f>
2013-07-18 14:38:57 +0000

    d n d

--- trunk/web-terminal/js/view.js
+++ trunk/web-terminal/js/view.js
@@ -86,14 +86,14 @@ function View(arrow, terminal, d) {
 
             self.terminal.dragStartX = event.originalEvent.screenX;
             self.terminal.dragStartY = event.originalEvent.screenY;
-            self.children('input').css('visibility', 'hidden');
+            self.d.children('input').css('visibility', 'hidden');
             event.originalEvent.dataTransfer.setData('text/plain', self.d.val());
             event.originalEvent.dataTransfer.effectAllowed = "move";
             event.stopPropagation();
         },
         
         dragend: function(event) {
-            self.children('input').css('visibility', 'visible');
+            self.d.children('input').css('visibility', 'visible');
             if (self.terminal.dragOver) {
                 if (self.terminal.dragOver != self) {
                     self.terminal.dragOver.replaceWith(self);
