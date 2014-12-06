23faa3e3ac3340f86c3573e4e44e54ed9198d157
sylvain.garden <sylvain.garden@479a42bd-9052-0410-8fdd-918bd02b771f>
2014-12-06 13:01:57 +0000

    default port 8080 because cloud9

--- cli.sh
+++ cli.sh
@@ -8,10 +8,10 @@ HEADERS=headers.tmp
 do
    echo -n '>> '
    curl -b $COOKIES -c $COOKIES -D $HEADERS \
-     -s "http://localhost:8008/$line"
+     -s "http://localhost:8080/$line"
    session=$(sed -ne '/session/ s/.*\t//p' "$COOKIES")
    location=$(sed -ne '/Content-Location/ s/.* \([^\r]*\).*/\1/p' "$HEADERS")
    echo " << s="\"$session\"" u="\"$location\"
    #cat $HEADERS
 done
-
+rm "$COOKIES"
