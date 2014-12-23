76b3b779280fe5b99bc387ddbc3e8379328d7e34
sylvain.garden <sylvain.garden@479a42bd-9052-0410-8fdd-918bd02b771f>
2014-12-23 01:16:35 +0000

    load the demo context with initial data

--- trunk/web-terminal/load_pub.sh
+++ trunk/web-terminal/load_pub.sh
@@ -7,5 +7,5 @@
 /escalate/escape//mudo+chut//fall+/escape+demo/,/land+
 /linkTailWithHead/escape/pub+help
 /linkTailWithHead/escape/pub+hit%20e%20under%20help%20to%20get%20some%20help
-/macro/x/arrow/linkTailWithHead//escape+escape/help//Content-Typed/text%2Fx-creole+var+x@@@help.wiki
+/macro/x/arrow/linkTailWithHead//escape+escape/help+var+x@@@help.wiki
 FIN
